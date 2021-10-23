ff61fc0eeae64bc305f6081ad6d978ad804661fd
bHaRatH9035 <bharath.m.9035@gmail.com>
2021-10-23 18:58:37 +0530

    2 files modified, 1 added

---
+++ src/ThemeSwitcher.js
@@ -0,0 +1,28 @@
+import { Switch } from 'antd';
+import React, { useEffect, useState } from 'react';
+
+import LIGHT from 'antd/dist/antd.css';
+import DARK from 'antd/dist/antd.css';
+
+export const ThemeSwitcher = () => {
+  const [dark, setDark] = useState(true);
+
+  useEffect(() => {
+    if (dark) {
+      LIGHT.unuse();
+      DARK.use();
+    } else {
+      DARK.unuse();
+      LIGHT.use();
+    }
+  }, [dark]);
+
+  return (
+    <Switch
+      checked={dark}
+      onChange={() => setDark(!dark)}
+      checkedChildren={'Light'}
+      unCheckedChildren={'Dark'}
+    />
+  );
+};
